ff1371128d9a32ec6a00c2ae26e3804fa1145e89
Swir <swirjestem@hotmail.com>
2018-03-17 19:21:37 +0000

    Update demo.php

--- demo.php
+++ demo.php
@@ -1,1 +1,1 @@
-<NAME>Add name in here</NAME><URL>Add url</URL><ICON>Add Icon</ICON><FANART>Add Fanart</FANART><DESC>Add description</DESC>
+<NAME>Filmy</NAME><URL>https://raw.githubusercontent.com/Swir/forum/master/vod2.txt</URL><ICON>Add Icon</ICON><FANART>Add Fanart</FANART><DESC>linki do filmow</DESC>
